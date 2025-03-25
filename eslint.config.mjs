5dd4fbdb186fa1389d85a6788992ee9ce38c3973
Lyle Oliver <oliverlyle29@gmail.com>
2025-03-25 15:31:30 +0200

    eslint error fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import reactRecommended from "eslint-plugin-react/configs/recommended.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +10,20 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    ...reactRecommended,
+    rules: {
+      "react/no-unescaped-entities": ["error", {
+        forbid: [">", "}"], // Only forbid specific HTML entities
+      }],
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "react/react-in-jsx-scope": "off"
+    }
+  }
+];
 
 export default eslintConfig;
